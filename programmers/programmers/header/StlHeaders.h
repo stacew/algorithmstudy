e8cb6dc37be8196e93d864044319f8d4289f83f6
stacew <chunrans4@naver.com>
2021-02-16 23:23:30 +0900

    test setting

--- programmers/programmers/header/StlHeaders.h
+++ programmers/programmers/header/StlHeaders.h
@@ -1,15 +1,13 @@
 #pragma once
 
 #include <iostream>
-#include <algorithm> //sort, partial_sort
-
-#include <string> //to_string(number)
+#include <string>
 #include <vector>
 #include <array>
 #include <unordered_map>
 #include <unordered_set>
 #include <queue> //priority_queue
-
+#include <algorithm> //sort, partial_sort
 #include <cmath>//pow, sqrt, abs, log, sin, cos, tan
 #include <stack>
 #include <list>
