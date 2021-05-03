59e6f52ffe0d9daa21b374bfbdcde09b8604a7f8
stacew <chunrans4@naver.com>
2021-05-04 05:01:29 +0900

    update

--- programmers/header/기타/RPG와 쿼리.h
+++ programmers/header/기타/RPG와 쿼리.h
@@ -1,4 +1,118 @@
 #pragma once
+// dp로 가야 됨.
+
+
+int inPlaceGold;
+using LL = long long;
+unordered_map<int, unordered_map<int, LL>> gp;  //from, to, gold
+unordered_map<LL, vector<pair< int, int >> > goldTo;//gold, to
+void buildInfo(int n, vector<vector<int>>& roads, int z) {
+	inPlaceGold = z;
+
+	//build graph & goldTo
+	gp.clear();
+	goldTo.clear();
+	for (const auto& road : roads) {
+		auto from = road[0];
+		auto to = road[1];
+		auto gold = road[2];
+		gp[from][to] = gold;
+
+		goldTo[gold].push_back(make_pair(from, to));
+	}
+}
+
+LL getMinTurn(LL needGold, int from) {
+	if (needGold == 0)
+		return 0;
+
+	LL minTurn = -1;
+
+	// 1
+	if (needGold >= inPlaceGold) {
+		minTurn = getMinTurn(needGold - inPlaceGold, from);
+	}
+	if (minTurn != -1)
+		return minTurn + 1;
+
+	// 2
+	LL minTurn1 = -1;
+	for (const auto& ng : gp[from]) {
+		auto to = ng.first;
+		auto gold = ng.second;
+		if (needGold < gold)
+			continue;
+
+		LL getSubMin = getMinTurn(needGold - gold, to);
+		if (getSubMin == -1)
+			continue;
+
+		if (minTurn1 == -1 || minTurn1 > getSubMin)
+			minTurn1 = getSubMin;
+	}
+	if (minTurn1 != -1)
+		minTurn1 += 1;
+
+	// 3
+	for (const auto& gt : goldTo) {
+		auto gold = gt.first;
+		if (needGold < gold)
+			continue;
+
+		auto ftVec = gt.second;
+		int checkFrom;
+		for (const auto& ft : ftVec) {
+			LL getSubMin = getMinTurn(needGold - gold, ft.second);
+			if (getSubMin == -1)
+				continue;
+
+			if (minTurn == -1 || minTurn > getSubMin) {
+				minTurn = getSubMin;
+				checkFrom = ft.first;
+			}
+
+		}
+		if (minTurn != -1) {
+			minTurn += ((checkFrom == from) ? 1 : 2);
+			break;
+		}
+	}
+
+	if (minTurn1 > -1 && minTurn > -1)
+	{
+		if (minTurn1 < minTurn)
+			return minTurn1;
+		return minTurn;
+	}
+	if (minTurn1 > -1)
+		return minTurn1;
+	if (minTurn > -1)
+		return minTurn;
+	return -1;
+}
+
+vector<LL> solution(int n, int z, vector<vector<int>> roads, vector<LL> goldQuery100000s) {
+
+	buildInfo(n, roads, z);
+
+	vector<LL> answer;
+	for (auto& gold : goldQuery100000s) {
+		cout << gold << " : ";
+		LL mul = 0;
+		if (gold >= inPlaceGold * 2) {
+			mul = gold / inPlaceGold - 1;//1,2,3,4,5 test
+			gold -= mul * inPlaceGold;
+		}
+		LL min = getMinTurn(gold, 0);
+		answer.emplace_back(min + mul);
+		cout << min + mul << endl;
+	}
+	return answer;
+}
+
+
+#if 0 
+#pragma once
 
 using LL = long long;
 unordered_map<LL, unordered_map<LL, LL>> gp;  //from, to, gold
@@ -112,3 +226,5 @@ vector<LL> solution(int n, int z, vector<vector<int>> roads, vector<LL> goldQuer
 
 	return answer;
 }
+
+#endif
